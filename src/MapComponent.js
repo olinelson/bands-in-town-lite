45adbcf873ce7d0a868f35297aba4e957bd32e2c
olinelson <olivernelson@icloud.com>
2019-04-21 14:59:44 -0400

    added better formatting for map

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -7,23 +7,25 @@ const MapComponent = props => {
 
   return (
     <div className="map-fullscreen-container">
-    <button onClick={props.hideMap}>exit</button>
-      <LeafletMap
-        scrollWheelZoom={false}
-        id={"leaflet-map"}
-        center={position}
-        zoom={12}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </LeafletMap>
+      <div className="map-inner-container">
+        <button onClick={props.hideMap}>exit</button>
+        <LeafletMap
+          scrollWheelZoom={false}
+          id={"leaflet-map"}
+          center={position}
+          zoom={12}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </LeafletMap>
+      </div>
     </div>
   );
 };
