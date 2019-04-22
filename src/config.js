1c85263b9de51ff536b5ef8cf76c10e23efa65fc
olinelson <olivernelson@icloud.com>
2019-04-21 21:17:10 -0400

    f

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let API_URL;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env["NODE_ENV"] === "development") {
   API_URL = "http://localhost:3000/api/v1"
 } else {
   API_URL = "https://bands-in-town-lite-api.herokuapp.com/api/v1"
